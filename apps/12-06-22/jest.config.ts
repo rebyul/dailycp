7afa719719a452ab6627b4c0ce80c13c65398c24
rebyul <rebyul@gmail.com>
2022-06-12 21:50:38 +1200

    Update node to 18

---
+++ apps/12-06-22/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: '12-06-22',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/12-06-22',
+};
