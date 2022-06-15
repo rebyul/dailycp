cf5b6ff208295677e156880749557419f1a98bc0
rebyul <rebyul@gmail.com>
2022-06-16 04:43:04 +1200

    word construction

--- libs/dailycps/src/lib/wordConstruction/wordConstruction.spec.ts
+++ libs/dailycps/src/lib/wordConstruction/wordConstruction.spec.ts
@@ -1,10 +1,56 @@
-import { canConstruct } from './wordConstruction';
+import { allConstruct, canConstruct, countConstruct } from './wordConstruction';
 
-describe('Can construct', () => {
+describe('Word construction', () => {
   test.each([
     ['abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'], true],
     ['skateboard', ['ab', 'abc', 'cd', 'def', 'abcd'], false],
-  ])('.canConstruct', (word, pieces, result) => {
+    ['', ['ab', 'cd'], true],
+    [
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef',
+      ['e', 'ee', 'eee', 'eeee', 'eeeee'],
+      false,
+    ],
+  ])('.canConstruct(%s, %p)', (word, pieces, result) => {
     expect(canConstruct(word, pieces)).toEqual(result);
+  });
+
+  test.each([
+    ['abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'], 1],
+    ['skateboard', ['ab', 'abc', 'cd', 'def', 'abcd'], 0],
+    ['', ['ab', 'cd'], 1],
+    [
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef',
+      ['e', 'ee', 'eee', 'eeee', 'eeeee'],
+      0,
+    ],
+  ])('.countConstruct(%s, %p)', (word, pieces, result) => {
+    expect(countConstruct(word, pieces)).toEqual(result);
+  });
+  test.each([
+    [
+      'purple',
+      ['purp', 'p', 'ur', 'le', 'purpl'],
+      [
+        ['purp', 'le'],
+        ['p', 'ur', 'p', 'le'],
+      ],
+    ],
+    [
+      'abcdef',
+      ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c'],
+      [
+        ['ab', 'cd', 'ef'],
+        ['ab', 'c', 'def'],
+        ['abc', 'def'],
+        ['abcd', 'ef'],
+      ],
+    ],
+    [
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef',
+      ['e', 'ee', 'eee', 'eeee', 'eeeee'],
+      [],
+    ],
+  ])('.allConstruct(%s, %p)', (word, pieces, output) => {
+    expect(allConstruct(word, pieces)).toEqual(output);
   });
 });
--- libs/dailycps/src/lib/wordConstruction/wordConstruction.ts
+++ libs/dailycps/src/lib/wordConstruction/wordConstruction.ts
@@ -1,10 +1,69 @@
-export function canConstruct(word: string, pieces: string[]): boolean {
-  if (word === '') return true;
+export function canConstruct(
+  word: string,
+  pieces: string[],
+  memo: { [key: string]: boolean } = {}
+): boolean {
+  if (word in memo) return memo[word];
+  if (word.length === 0) return true;
 
-  for (const piece of pieces.filter((p) => p.charAt(0) === word.charAt(0))) {
-    if (canConstruct(piece.substring(piece.length - 1, word.length), pieces))
+  for (const piece of pieces.filter((p) => word.indexOf(p) === 0)) {
+    const substring = word.slice(piece.length);
+
+    if (canConstruct(substring, pieces, memo) === true) {
+      memo[word] = true;
       return true;
+    }
+  }
+  memo[word] = false;
+  return false;
+}
+
+export function countConstruct(
+  word: string,
+  pieces: string[],
+  memo: { [key: string]: number } = {}
+): number {
+  if (word in memo) return memo[word];
+  if (word === '') {
+    return 1;
   }
 
-  return false;
+  let count = 0;
+
+  for (const piece of pieces) {
+    if (word.indexOf(piece) === 0) {
+      const substring = word.slice(piece.length);
+
+      count += countConstruct(substring, pieces, memo);
+    }
+  }
+  memo[word] = count;
+  return count;
+}
+
+export function allConstruct(
+  word: string,
+  pieces: string[],
+  memo: { [key: string]: string[][] } = {}
+) {
+  if (word in memo) return memo[word];
+  if (word === '') {
+    return [[]];
+  }
+
+  const combinations: string[][] = [];
+
+  for (const piece of pieces) {
+    if (word.indexOf(piece) === 0) {
+      const substring = word.slice(piece.length);
+      const suffixWays = allConstruct(substring, pieces, memo);
+      if (suffixWays !== null) {
+        const prefixWays = suffixWays.map((way) => [piece, ...way]);
+        combinations.push(...prefixWays);
+      }
+    }
+  }
+
+  memo[word] = combinations;
+  return memo[word];
 }
