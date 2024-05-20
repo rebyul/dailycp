6fb6c4708bd987582f294218df5d3857d576e9d4
rebyul <rebyul@gmail.com>
2024-05-21 00:27:59 +1200

    Make number generation more efficient

--- libs/dailycps/src/problems/may-24/04-05/primesSmallerThanN.ts
+++ libs/dailycps/src/problems/may-24/04-05/primesSmallerThanN.ts
@@ -13,11 +13,9 @@
 // Bonus: Create a generator that produces primes indefinitely (that is, without taking N as an input).
 
 export function findPrimes(input: number): number[] {
-  // Enumerate all numbers from 2 to input
-  const numbers = enumerateNumbers(input);
-  // Filter 0 and 1 as they are non prime numbers by definition
-  const potentialPrimes = new Set(filterZero(filterOne(numbers)));
+  if (input === 0 || input === 1) return [];
 
+  const potentialPrimes = generatePotentialPrimes(input);
   // Only generate multiple factors that are less than half the input
   // Any higher multiples greater than half the input are too large, so ignore them.
   const maxInputFactor = Math.floor(input / 2);
@@ -35,17 +33,23 @@ export function findPrimes(input: number): number[] {
   return Array.from(potentialPrimes.values());
 }
 
+function generatePotentialPrimes(input: number): Set<number> {
+  if (input === 0) return new Set();
+  if (input === 1) return new Set([1]);
+  return new Set(Array.from({ length: input - 1 }, (_, index) => index + 2));
+}
+
 export function enumerateNumbers(size: number): number[] {
   return [...Array(size)].map((_, i) => i + 1);
 }
 
-function filterZero(numbers: number[]): number[] {
-  return numbers.filter((n) => n !== 0);
-}
-
-function filterOne(numbers: number[]): number[] {
-  return numbers.filter((n) => n !== 1);
-}
+// function filterZero(numbers: number[]): number[] {
+//   return numbers.filter((n) => n !== 0);
+// }
+//
+// function filterOne(numbers: number[]): number[] {
+//   return numbers.filter((n) => n !== 1);
+// }
 
 export function enumerateMultiplesBelow(seed: number, max: number): number[] {
   if (seed === 0 || seed === 1)
