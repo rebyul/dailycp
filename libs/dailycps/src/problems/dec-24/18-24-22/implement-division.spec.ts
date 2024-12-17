430e49a8af73cfda9481236b46bc4f63e92e53c9
rebyul <rebyul@gmail.com>
2024-12-18 11:50:11 +1300

    Formatting

--- libs/dailycps/src/problems/dec-24/18-24-22/implement-division.spec.ts
+++ libs/dailycps/src/problems/dec-24/18-24-22/implement-division.spec.ts
@@ -6,7 +6,7 @@ describe('Implement division', () => {
     [2, 1, 2],
     [98234729387, 234, 419806535],
   ])(
-    'divide %s by %s returns %s as quotient',
+    'divide %d by %d returns %d as quotient',
     (denominator, nominator, quotient) => {
       expect(divide(denominator, nominator)).toEqual(quotient);
     }
