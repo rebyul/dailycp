3884ca7d90a1d15c30f270f82ad01fd74d068d79
rebyul <rebyul@gmail.com>
2024-12-18 13:56:38 +1300

    Even array splitting

--- libs/dailycps/src/problems/dec-24/17-12-24/deleting-palindromes.spec.ts
+++ libs/dailycps/src/problems/dec-24/17-12-24/deleting-palindromes.spec.ts
@@ -1,11 +1,25 @@
-import { isPalindrome } from './deleting-palindromes';
+import { isPalindrome, splitArrayEvenly } from './deleting-palindromes';
 
 describe('Deleting palindromes', () => {
-  test.each([
+  test.concurrent.each([
     ['watterrfetawx', false],
     ['waterretaw', true],
     ['pop', true],
   ])('%s is a palindrome: %s', (word, result) => {
     expect(isPalindrome(word)).toEqual(result);
+  });
+
+  test('Split even length array evenly', () => {
+    expect(splitArrayEvenly([1, 2, 3, 4])).toEqual([
+      [1, 2],
+      [3, 4],
+    ]);
+  });
+
+  test('Split odd length array evenly', () => {
+    expect(splitArrayEvenly([1, 2, 3, 4, 5])).toEqual([
+      [1, 2],
+      [4, 5],
+    ]);
   });
 });
--- libs/dailycps/src/problems/dec-24/17-12-24/deleting-palindromes.ts
+++ libs/dailycps/src/problems/dec-24/17-12-24/deleting-palindromes.ts
@@ -2,6 +2,10 @@ export function deletingPalindromes(
   word: string,
   maxDeletes: number
 ): string | null {
+  if (isPalindrome(word)) {
+    return word;
+  }
+
   return null;
 }
 
@@ -10,13 +14,20 @@ export function isPalindrome(word: string): boolean {
     if (word[i] === word[word.length - 1 - i]) {
       continue;
     }
-    console.error(
-      `${word}: At ${i} ${word[i]} and ${
-        word[word.length - 1 - i]
-      } are not equal`
-    );
+    // console.error(
+    //   `${word}: At ${i} ${word[i]} and ${
+    //     word[word.length - 1 - i]
+    //   } are not equal`
+    // );
     return false;
   }
 
   return true;
+}
+
+export function splitArrayEvenly<T>(arr: Array<T>): [Array<T>, Array<T>] {
+  return [
+    arr.slice(0, Math.floor(arr.length / 2)),
+    arr.slice(Math.ceil(arr.length / 2), arr.length),
+  ];
 }
