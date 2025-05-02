f7d2c353d36c04ea5596d299294273c2cac7a9a3
rebyul <rebyul@gmail.com>
2025-05-03 02:59:17 +1200

    Upgrade eslint to flat config
    Command was nx generate convert-to-flat-config

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import nxEslintPlugin from '@nx/eslint-plugin';
+
+export default [
+  {
+    ignores: ['**/dist'],
+  },
+  { plugins: { '@nx': nxEslintPlugin } },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+];
