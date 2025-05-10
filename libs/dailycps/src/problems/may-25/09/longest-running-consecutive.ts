469d4454344948d0a183a470696a28dc1fd9b08d
rebyul <rebyul@gmail.com>
2025-05-11 01:22:25 +1200

    Im an idiot. Use the set you created..

--- libs/dailycps/src/problems/may-25/09/longest-running-consecutive.ts
+++ libs/dailycps/src/problems/may-25/09/longest-running-consecutive.ts
@@ -12,7 +12,7 @@ export function longestConsecutive(nums: number[]): number {
   const set = new Set(nums);
   let max = 0;
 
-  for (const num of nums) {
+  for (const num of set) {
     if (set.has(num + 1)) continue;
 
     let counter = 1,
