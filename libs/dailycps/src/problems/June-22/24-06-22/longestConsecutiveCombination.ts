7cf036a29f0bacb23e6f71fc6818d7ffc0428f10
rebyul <rebyul@gmail.com>
2025-05-11 00:53:23 +1200

    Formatting

--- libs/dailycps/src/problems/June-22/24-06-22/longestConsecutiveCombination.ts
+++ libs/dailycps/src/problems/June-22/24-06-22/longestConsecutiveCombination.ts
@@ -1,7 +1,9 @@
 /**
- * Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+ * Given an integer k and a string s, find the length of the longest substring
+ * that contains at most k distinct characters.
 
-For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+For example, given s = "abcba" and k = 2, the longest substring with k distinct
+characters is "bcb".
  */
 
 export function recursiveLongestConsecutiveCombination(
