05e863ca7ad125390d047519e16ffcff316f7fdd
rebyul <rebyul@gmail.com>
2025-05-21 23:20:50 +1200

    one moment pls

--- libs/dailycps/src/problems/may-25/11/minCoins.spec.ts
+++ libs/dailycps/src/problems/may-25/11/minCoins.spec.ts
@@ -1,40 +1,70 @@
-import { minCoins, harderMinCoins } from './minCoins';
+import { minCoins, harderMinCoins, takeNotakeCoins } from "./minCoins";
 
-describe('Min coins', () => {
-  test('Input 0 returns 0', () => {
-    expect(minCoins(0)).toEqual(0);
-  });
-  test.each([[16, 3]])('Given maxCoin(%d) returns %d)', (max, output) => {
-    expect(minCoins(max)).toEqual(output);
-  });
+describe("Min coins", () => {
+	test("Input 0 returns 0", () => {
+		expect(minCoins(0)).toEqual(0);
+	});
+	test.each([[16, 3]])("Given maxCoin(%d) returns %d)", (max, output) => {
+		expect(minCoins(max)).toEqual(output);
+	});
 });
 
-describe('Harder min coins', () => {
-  test('No coins returns -1', () => {
-    expect(harderMinCoins([], 5)).toEqual(-1);
-  });
+describe("Harder min coins", () => {
+	test("No coins returns -1", () => {
+		expect(harderMinCoins([], 5)).toEqual(-1);
+	});
 
-  test('Impossible combination returns -1', () => {
-    expect(harderMinCoins([4, 5, 6], 3)).toEqual(-1);
-  });
+	test("Impossible combination returns -1", () => {
+		expect(harderMinCoins([4, 5, 6], 3)).toEqual(-1);
+	});
 
-  test('Sum 0 returns 0', () => {
-    expect(harderMinCoins([1, 2, 3], 0)).toEqual(0);
-  });
+	test("Sum 0 returns 0", () => {
+		expect(harderMinCoins([1, 2, 3], 0)).toEqual(0);
+	});
 
-  test.each([
-    [[10], 40, 4],
-    [[25, 10, 5], 30, 2],
-    [[9, 6, 5, 1], 19, 3],
-    [[186, 419, 83, 408], 6249, 20],
-  ])(
-    'Possible combination harderMinCoins(%s, %d) returns %d',
-    (input, total, expected) => {
-      expect(harderMinCoins(input, total)).toEqual(expected);
-    }
-  );
+	test.each([
+		[[10], 40, 4],
+		[[25, 10, 5], 30, 2],
+		[[9, 6, 5, 1], 19, 3],
+		[[186, 419, 83, 408], 6249, 20],
+	])(
+		"Possible combination harderMinCoins(%s, %d) returns %d",
+		(input, total, expected) => {
+			expect(harderMinCoins(input, total)).toEqual(expected);
+		},
+	);
 
-  test('meow', () => {
-    expect(harderMinCoins([3, 6, 7, 8, 4, 1], 4)).toEqual(1);
-  });
+	test("meow", () => {
+		expect(harderMinCoins([3, 6, 7, 8, 4, 1], 4)).toEqual(1);
+	});
+});
+
+describe("Geekcode solution attempt", () => {
+	test("No coins returns -1", () => {
+		expect(takeNotakeCoins([], 5)).toEqual(-1);
+	});
+
+	test("Impossible combination returns -1", () => {
+		expect(takeNotakeCoins([4, 5, 6], 3)).toEqual(-1);
+	});
+
+	test("Sum 0 returns 0", () => {
+		expect(takeNotakeCoins([1, 2, 3], 0)).toEqual(0);
+	});
+
+	test.each([
+		[[10], 40, 4],
+		[[25, 10, 5], 30, 2],
+		[[9, 6, 5, 1], 19, 3],
+		[[186, 419, 83, 408], 6249, 20],
+	])(
+		"Possible combination takeNotakeCoins(%s, %d) returns %d",
+		(input, total, expected) => {
+			expect(takeNotakeCoins(input, total)).toEqual(expected);
+		},
+	);
+
+	test("meow", () => {
+		expect(takeNotakeCoins([3, 6, 7, 8, 4, 1], 4)).toEqual(1);
+	});
 });
--- libs/dailycps/src/problems/may-25/11/minCoins.ts
+++ libs/dailycps/src/problems/may-25/11/minCoins.ts
@@ -8,84 +8,82 @@ You can use standard American denominations, that is, 1¢, 5¢, 10¢, and 25¢.
 For example, given n = 16, return 3 since we can make it with a 10¢, a 5¢, and a 1¢.
  */
 export function minCoins(total: number): number {
-  let localTotal = total,
-    coins = 0;
+	let localTotal = total,
+		coins = 0;
 
-  while (localTotal > 0) {
-    if (localTotal >= 25) {
-      localTotal -= 25;
-    } else if (localTotal >= 10) {
-      localTotal -= 10;
-    } else if (localTotal >= 5) {
-      localTotal -= 5;
-    } else {
-      localTotal -= 1;
-    }
-    coins++;
-  }
+	while (localTotal > 0) {
+		if (localTotal >= 25) {
+			localTotal -= 25;
+		} else if (localTotal >= 10) {
+			localTotal -= 10;
+		} else if (localTotal >= 5) {
+			localTotal -= 5;
+		} else {
+			localTotal -= 1;
+		}
+		coins++;
+	}
 
-  return coins;
+	return coins;
 }
 
 export function harderMinCoins(coins: number[], total: number): number {
-  const cache = new Map();
-  harderMinCoinsHelper(coins, total, 0, 0, cache);
-  const result = cache.get(total);
-  return Number.isInteger(result) ? result : -1;
+	const cache = new Map();
+	harderMinCoinsHelper(coins, total, 0, 0, cache);
+	const result = cache.get(total);
+	return Number.isInteger(result) ? result : -1;
 }
 
 function harderMinCoinsHelper(
-  coins: number[],
-  target: number,
-  currentSum: number,
-  depth: number,
-  cache: Map<number, number>
+	coins: number[],
+	target: number,
+	currentSum: number,
+	depth: number,
+	cache: Map<number, number>,
 ) {
-  if (!coins || currentSum > target) {
-    updateCache(cache, currentSum, -1);
-    return;
-  }
+	if (!coins || currentSum > target) {
+		updateCache(cache, currentSum, -1);
+		return;
+	}
 
-  // Hit jackpot
-  if (currentSum === target) {
-    updateCache(cache, currentSum, depth);
-    return;
-  }
+	// Hit jackpot
+	if (currentSum === target) {
+		updateCache(cache, currentSum, depth);
+		return;
+	}
 
-  // See if we've been to where we're going
-  const existingCache = cache.get(currentSum);
+	// See if we've been to where we're going
+	const existingCache = cache.get(currentSum);
 
-  if (existingCache === -1) {
-    return;
-  }
+	// If we've been there with a shorter or equal route. Stop here. We've done this
+	if (existingCache && (existingCache === -1 || existingCache <= depth)) {
+		return;
+	}
 
-  // If we've been there with a shorter or equal route. Stop here. We've done this
-  if (existingCache && existingCache <= depth) {
-    return;
-  }
+	// Our depth is lower than the existing cache.
+	// Keep going as we're discovering new heights
+	updateCache(cache, currentSum, depth);
 
-  // Our depth is lower than the existing cache.
-  // Keep going as we're discovering new heights
-  updateCache(cache, currentSum, depth);
+	for (const coin of coins) {
+		const nextSum = currentSum + coin;
 
-  for (const coin of coins) {
-    const nextSum = currentSum + coin;
+		harderMinCoinsHelper(coins, target, nextSum, depth + 1, cache);
+	}
 
-    harderMinCoinsHelper(coins, target, nextSum, depth + 1, cache);
-  }
-
-  return;
+	return;
 }
 
 function updateCache(
-  cache: Map<number, number>,
-  target: number,
-  depth: number
+	cache: Map<number, number>,
+	target: number,
+	depth: number,
 ): number {
-  const currentTargetMin = cache.get(target)!;
-  if (Number.isInteger(currentTargetMin) && currentTargetMin <= depth) {
-    return currentTargetMin;
-  }
-  cache.set(target, depth);
-  return depth;
+	const currentTargetMin = cache.get(target)!;
+	if (Number.isInteger(currentTargetMin) && currentTargetMin <= depth) {
+		return currentTargetMin;
+	}
+	cache.set(target, depth);
+	return depth;
 }
+
+export function takeNotakeCoins(coins: number[], total: number): number {}
