c6996686034ef78acd3eda9ff913e08c27902370
rebyul <rebyul@gmail.com>
2025-05-24 19:58:42 +1200

    Formatting

--- libs/dailycps/jest.config.ts
+++ libs/dailycps/jest.config.ts
@@ -1,15 +1,16 @@
+const meow: string = '';
 export default {
-	displayName: "dailycps",
-	preset: "../../jest.preset.js",
-	globals: {},
-	transform: {
-		"^.+\\.[tj]s$": [
-			"ts-jest",
-			{
-				tsconfig: "<rootDir>/tsconfig.spec.json",
-			},
-		],
-	},
-	moduleFileExtensions: ["ts", "js", "html"],
-	coverageDirectory: "../../coverage/libs/dailycps",
+  displayName: 'dailycps',
+  preset: '../../jest.preset.js',
+  globals: {},
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/dailycps',
 };
