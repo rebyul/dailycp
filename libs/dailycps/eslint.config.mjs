c2cc2aafce4012c3e35cba33939290f9f4a7d231
rebyul <rebyul@gmail.com>
2025-05-24 17:47:55 +1200

    Upgrade nx to 21.1.2

--- libs/dailycps/eslint.config.mjs
+++ libs/dailycps/eslint.config.mjs
@@ -1,39 +1,39 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import js from '@eslint/js';
-import baseConfig from '../../eslint.config.mjs';
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import js from "@eslint/js";
+import baseConfig from "../../eslint.config.mjs";
 
 const compat = new FlatCompat({
-  baseDirectory: dirname(fileURLToPath(import.meta.url)),
-  recommendedConfig: js.configs.recommended,
+	baseDirectory: dirname(fileURLToPath(import.meta.url)),
+	recommendedConfig: js.configs.recommended,
 });
 
 export default [
-  {
-    ignores: ['**/dist'],
-  },
-  ...baseConfig,
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    // Override or add rules here
-    rules: {},
-    languageOptions: {
-      parser: await import('@typescript-eslint/parser'),
-      parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 'latest',
-      },
-    },
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    // Override or add rules here
-    rules: {},
-  },
-  {
-    files: ['**/*.js', '**/*.jsx'],
-    // Override or add rules here
-    rules: {},
-  },
+	{
+		ignores: ["**/dist"],
+	},
+	...baseConfig,
+	{
+		files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+		// Override or add rules here
+		rules: {},
+		languageOptions: {
+			parser: await import("@typescript-eslint/parser"),
+			parserOptions: {
+				sourceType: "module",
+				ecmaVersion: "latest",
+			},
+		},
+	},
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		// Override or add rules here
+		rules: {},
+	},
+	{
+		files: ["**/*.js", "**/*.jsx"],
+		// Override or add rules here
+		rules: {},
+	},
 ];
--- libs/dailycps/jest.config.ts
+++ libs/dailycps/jest.config.ts
@@ -1,16 +1,15 @@
-/* eslint-disable */
 export default {
-  displayName: 'dailycps',
-  preset: '../../jest.preset.js',
-  globals: {},
-  transform: {
-    '^.+\\.[tj]s$': [
-      'ts-jest',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-      },
-    ],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/dailycps',
+	displayName: "dailycps",
+	preset: "../../jest.preset.js",
+	globals: {},
+	transform: {
+		"^.+\\.[tj]s$": [
+			"ts-jest",
+			{
+				tsconfig: "<rootDir>/tsconfig.spec.json",
+			},
+		],
+	},
+	moduleFileExtensions: ["ts", "js", "html"],
+	coverageDirectory: "../../coverage/libs/dailycps",
 };
