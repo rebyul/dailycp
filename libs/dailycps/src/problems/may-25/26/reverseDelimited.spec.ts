0b4c979969fff8ae429558375f24d5e55fc7a075
rebyul <rebyul@gmail.com>
2025-06-02 22:17:07 +1200

    Initial try. Worked but need to take case of edge cases

--- libs/dailycps/src/problems/may-25/26/reverseDelimited.spec.ts
+++ libs/dailycps/src/problems/may-25/26/reverseDelimited.spec.ts
@@ -1,3 +1,20 @@
+import { reverseDelimited } from './reverseDelimited';
+
 describe('Split delimited', () => {
-  test('No test', () => {});
+  test.each([
+    ['hello/world:here', 'here/world:hello'],
+    ['here/world:hello', 'hello/world:here'],
+    ['hello/world:here/', 'here/world:hello/'],
+    ['hello', 'hello'],
+    ['//', '//'],
+  ])('reverseDelimited(%s) returns %s', (input, expected) => {
+    expect(reverseDelimited(input)).toEqual(expected);
+  });
+
+  // I think this case is flawed
+  test('Follow-up', () => {
+    expect(reverseDelimited('hello/world:here/')).not.toEqual(
+      'hello//world:here'
+    );
+  });
 });
--- libs/dailycps/src/problems/may-25/26/reverseDelimited.ts
+++ libs/dailycps/src/problems/may-25/26/reverseDelimited.ts
@@ -7,3 +7,54 @@ return "here/world:hello"
 
 Follow-up: Does your solution work for the following cases: "hello/world:here/", "hello//world:here"
 */
+
+/**
+ * Thoughts
+ * no of delimiters < no of words
+ * why does the follow-up example print in that order?
+ * i guess it can't print a delimiter first? so prints the word first, but then
+ * shouldn't it print (blank)/here:world/hello?
+ */
+
+export function reverseDelimited(input: string): string {
+  if (input.length === 0) {
+    return '';
+  }
+  const delimiterSet = new Set(['/', ':']);
+
+  let currentWord = '';
+  const words = [];
+  const delimiters = [];
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+    if (delimiterSet.has(char)) {
+      delimiters.push(char);
+      // Add the current word to the words set
+      if (currentWord.length > 0) {
+        words.push(currentWord);
+        // Reset current word as we've met a delimiter
+        currentWord = '';
+      }
+    } else if (i === input.length - 1) {
+      if (currentWord.length > 0) {
+        currentWord += char;
+        words.push(currentWord);
+        // Reset current word as we've met a delimiter
+      }
+    } else {
+      currentWord += char;
+    }
+  }
+
+  const reversedWords = words.toReversed();
+
+  let outputString = '';
+  for (const word of reversedWords) {
+    outputString += word;
+    const nextDelimiter = delimiters.shift();
+    if (nextDelimiter !== undefined) {
+      outputString += nextDelimiter;
+    }
+  }
+  return outputString;
+}
