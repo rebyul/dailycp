45680d41cf246cd568b8600f320409834ba1d84a
rebyul <rebyul@gmail.com>
2025-07-15 01:02:17 +1200

    Format test name

--- libs/dailycps/src/problems/july-25/14-06/max-subarray-length-k.test.ts
+++ libs/dailycps/src/problems/july-25/14-06/max-subarray-length-k.test.ts
@@ -1,10 +1,10 @@
 import { maxSubarrayLengthK } from './max-subarray-length-k';
 
 describe('max subarray length k', () => {
-  test.each([[[10, 5, 2, 7, 8, 7], 3, [10, 7, 8, 8]]])(
-    'test',
-    (input, k, expected) => {
-      expect(maxSubarrayLengthK(input, k)).toEqual(expected);
-    }
-  );
+  test.each([
+    [[10, 5, 2, 7, 8, 7], 3, [10, 7, 8, 8]],
+    [[1, 2, 3, 4, 5, 6, 7, 8], 5, [5, 6, 7, 8]],
+  ])('given input=%p and k=%d, returns %p', (input, k, expected) => {
+    expect(maxSubarrayLengthK(input, k)).toEqual(expected);
+  });
 });
