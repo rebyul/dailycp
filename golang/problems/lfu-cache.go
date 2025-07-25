4f9e611037058b25b77dde16c3175c9d7f620e57
rebyul <rebyul@gmail.com>
2025-07-26 02:09:32 +1200

    Lfu in golang
    Got a loooooooooong way to go :) but its really fun and i get to play
    around with pointers.
    Didn't have any pointer related issues (ty lsp) maybe i'm playing in
    easy mode with it on. It fixed variable assignments to address
    assignments for me. Also just golang in general deals with dereferencing
    for everyone which is quite nice.

--- golang/problems/lfu-cache.go
+++ golang/problems/lfu-cache.go
@@ -1,6 +1,8 @@
 package problems
 
-import "errors"
+import (
+	"fmt"
+)
 
 type DoublyLinkedNode[K, T comparable] struct {
 	Key   K
@@ -8,6 +10,15 @@ type DoublyLinkedNode[K, T comparable] struct {
 	Freq  uint
 	Prev  *DoublyLinkedNode[K, T]
 	Next  *DoublyLinkedNode[K, T]
+}
+
+func NewDoublyLinkedNode[K, V comparable](key K, value V) *DoublyLinkedNode[K, V] {
+	return &DoublyLinkedNode[K, V]{Key: key,
+		Value: value,
+		Freq:  1,
+		Prev:  nil,
+		Next:  nil,
+	}
 }
 
 type DoublyLinkedList[K, T comparable] struct {
@@ -34,7 +45,7 @@ func (d *DoublyLinkedList[K, T]) RemoveNode(node *DoublyLinkedNode[K, T]) {
 	if node.Prev == nil {
 		d.Head = node.Next
 	} else {
-		node.Next.Prev = node.Prev
+		node.Prev.Next = node.Next
 	}
 
 	if node.Next == nil {
@@ -48,13 +59,117 @@ func (d *DoublyLinkedList[K, T]) RemoveNode(node *DoublyLinkedNode[K, T]) {
 	d.Count--
 }
 
-func (d *DoublyLinkedList[K, T]) RemoveTail() (*DoublyLinkedNode[K, T], error) {
+func (d *DoublyLinkedList[K, T]) RemoveTail() *DoublyLinkedNode[K, T] {
 	if d.Tail == nil {
-		return nil, errors.New("no tail")
+		return nil
 	}
 	tail := d.Tail
 
 	d.RemoveNode(d.Tail)
 
-	return tail, nil
+	return tail
+}
+
+/**
+ *Implement an LFU (Least Frequently Used) cache. It should be able to be
+ initialized with a cache size n, and contain the following methods:
+
+set(key, value): sets key to value. If there are already n items in the cache
+and we are adding a new item, then it should also remove the least frequently
+used item. If there is a tie, then the least recently used key should be
+removed. get(key): gets the value at key. If no such key exists, return null.
+*/
+
+type LfuCache[K, V comparable] struct {
+	MaxCount uint
+	Count    uint
+	minFreq  uint
+	cache    map[K](*DoublyLinkedNode[K, V])
+	freqMap  map[uint64](*DoublyLinkedList[K, V])
+}
+
+func InitLfuCache[K, V comparable](maxCount uint) (*LfuCache[K, V], error) {
+	if maxCount <= 0 {
+		return nil, fmt.Errorf("max count must be greater than 0")
+	}
+
+	return &LfuCache[K, V]{
+		MaxCount: maxCount,
+		minFreq:  0,
+		Count:    0,
+		cache:    make(map[K](*DoublyLinkedNode[K, V]), maxCount),
+		freqMap:  make(map[uint64](*DoublyLinkedList[K, V]), maxCount),
+	}, nil
+}
+
+func (c *LfuCache[K, V]) Get(key K) (V, bool) {
+	v, ok := c.cache[key]
+
+	if !ok {
+		var defaultV V
+		return defaultV, false
+	}
+
+	c.updateFrequency(v)
+
+	return v.Value, true
+}
+
+func (c *LfuCache[K, V]) Set(key K, value V) {
+	// If it exists override the value
+	existing, ok := c.cache[key]
+	if ok {
+		existing.Value = value
+		// Increase frequency
+		c.updateFrequency(existing)
+		return
+	} else {
+		// If count >= max, evict lfu node
+		if c.Count >= c.MaxCount {
+			currentMinFreq := c.freqMap[uint64(c.minFreq)]
+			removed := currentMinFreq.RemoveTail()
+			if removed != nil {
+				delete(c.cache, removed.Key)
+				c.Count--
+			}
+		}
+
+		// Insert if it doesn't exist
+		newNode := NewDoublyLinkedNode(key, value)
+		// Save new value in cache
+		c.cache[key] = newNode
+		// new nodes always have freq 1 so add to head of freqMap[1]
+		oneFreqMap, ok := c.freqMap[1]
+		if !ok {
+			oneFreqMap = new(DoublyLinkedList[K, V])
+			c.freqMap[1] = oneFreqMap
+		}
+		oneFreqMap.AddToHead(newNode)
+		// Reset minFrequency
+		c.minFreq = 1
+		// Increase count
+		c.Count++
+	}
+}
+
+func (c *LfuCache[K, V]) updateFrequency(node *DoublyLinkedNode[K, V]) {
+	// Find existing freqMap linked list
+	freqList := c.freqMap[uint64(node.Freq)]
+	// It has to exist. Remove it from the list
+	freqList.RemoveNode(node)
+
+	if freqList.Count == 0 && c.minFreq == node.Freq {
+		c.minFreq++
+	}
+
+	// Increment node freq
+	node.Freq++
+	// Find freqMap[existing freq +1]
+	newFreqList, ok := c.freqMap[uint64(node.Freq)]
+	if !ok {
+		newFreqList = new(DoublyLinkedList[K, V])
+		c.freqMap[uint64(node.Freq)] = newFreqList
+	}
+	// Add it to the head
+	newFreqList.AddToHead(node)
 }
--- golang/problems/lfu-cache_test.go
+++ golang/problems/lfu-cache_test.go
@@ -36,11 +36,11 @@ func TestDoublyLinkList_RemoveTail(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, gotErr := tt.list.RemoveTail()
+			got := tt.list.RemoveTail()
 
-			if gotErr != nil {
+			if got == nil {
 				if !tt.wantErr {
-					t.Errorf("RemoveTail() failed: %v", gotErr)
+					t.Errorf("RemoveTail() failed: %v", nil)
 				}
 				return
 			}
@@ -49,6 +49,163 @@ func TestDoublyLinkList_RemoveTail(t *testing.T) {
 			}
 			if node.Value != got.Value || node.Key != got.Key {
 				t.Errorf("RemoveTail() = %v, want %v", got.Value, tt.want.Value)
+			}
+		})
+	}
+}
+
+func TestLfuCache(t *testing.T) {
+	type K string
+	type V int
+
+	type getaction struct {
+		key     K
+		want    V
+		wantErr bool
+	}
+
+	type setaction struct {
+		key   K
+		value V
+	}
+
+	type cacheaction any
+
+	tests := []struct {
+		name     string // description of this test case
+		maxCount uint
+		actions  []cacheaction
+		wantErr  bool
+	}{
+		{
+			name:     "InitLfuCache succeeds with max count >0",
+			maxCount: 5,
+			wantErr:  false,
+		},
+		{
+			name:     "InitLfuCache fails with max count 0",
+			maxCount: 0,
+			wantErr:  true,
+		},
+		{
+			name:     "insert one item",
+			maxCount: 3,
+			wantErr:  false,
+			actions: []cacheaction{
+				setaction{key: "one", value: 1},
+				getaction{key: "one", want: 1, wantErr: false},
+			},
+		},
+		{
+			name:     "get non existent item",
+			maxCount: 3,
+			wantErr:  false,
+			actions: []cacheaction{
+				setaction{key: "one", value: 1},
+				getaction{key: "two", want: 0, wantErr: true},
+			},
+		},
+		{
+			name:     "test eviction",
+			maxCount: 3,
+			wantErr:  false,
+			actions: []cacheaction{
+				setaction{key: "one", value: 1},
+				setaction{key: "two", value: 2},
+				setaction{key: "three", value: 3},
+				getaction{key: "three", want: 3, wantErr: false},
+				setaction{key: "four", value: 4},
+				getaction{key: "one", want: 0, wantErr: true},
+				getaction{key: "four", want: 4, wantErr: false},
+			},
+		},
+		{
+			name:     "test updating keys correctly updates frequency",
+			maxCount: 3,
+			wantErr:  false,
+			actions: []cacheaction{
+				setaction{key: "one", value: 1},
+				setaction{key: "two", value: 2},
+				setaction{key: "three", value: 3},
+				getaction{key: "one", want: 1, wantErr: false},
+				setaction{key: "one", value: 2},
+				getaction{key: "one", want: 2, wantErr: false},
+				getaction{key: "two", want: 2, wantErr: false},
+				getaction{key: "three", want: 3, wantErr: false},
+			},
+		},
+		{
+			name:     "test evict all",
+			maxCount: 3,
+			wantErr:  false,
+			actions: []cacheaction{
+				setaction{key: "one", value: 1},
+				setaction{key: "two", value: 2},
+				setaction{key: "three", value: 3},
+				// Check 1,2,3 exists
+				getaction{key: "one", want: 1, wantErr: false},
+				getaction{key: "two", want: 2, wantErr: false},
+				getaction{key: "three", want: 3, wantErr: false},
+				// Set 4,5,6
+				// Immediately get to increase frequency to 2
+				setaction{key: "four", value: 4},
+				getaction{key: "four", want: 4, wantErr: false},
+				setaction{key: "five", value: 5},
+				getaction{key: "five", want: 5, wantErr: false},
+				setaction{key: "six", value: 6},
+				getaction{key: "six", want: 6, wantErr: false},
+				// Check 4,5,6 exists
+				getaction{key: "four", want: 4, wantErr: false},
+				getaction{key: "five", want: 5, wantErr: false},
+				getaction{key: "six", want: 6, wantErr: false},
+				// Check 1,2,3 have been evicted
+				getaction{key: "one", want: 0, wantErr: true},
+				getaction{key: "two", want: 0, wantErr: true},
+				getaction{key: "three", want: 0, wantErr: true},
+			},
+		},
+	}
+
+	execActions := func(cache *problems.LfuCache[K, V], a cacheaction) {
+		switch v := a.(type) {
+		case getaction:
+			{
+				got, ok := cache.Get(v.key)
+
+				if !ok {
+					if !v.wantErr {
+						t.Errorf("cache.Get(\"%v\") failed\nList status: %v\n", v.key, cache)
+					}
+					return
+				}
+
+				if v.wantErr {
+					t.Fatalf("cache.Get(\"%v\") succeeded unexpectedly\nList status: %v", v.key, cache)
+				}
+
+				if got != v.want {
+					t.Errorf("cache.Get(\"%v\") = %v, want %v\nList status: %v\n", v.key, got, v.want, cache)
+				}
+			}
+		case setaction:
+			{
+				cache.Set(v.key, v.value)
+			}
+		default:
+			t.Fatalf("unknown action type: %t", a)
+		}
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c, err := problems.InitLfuCache[K, V](tt.maxCount)
+
+			if err != nil && !tt.wantErr {
+				t.Errorf("InitLfuCache failed: %v", err)
+			}
+
+			for _, a := range tt.actions {
+				execActions(c, a)
 			}
 		})
 	}
