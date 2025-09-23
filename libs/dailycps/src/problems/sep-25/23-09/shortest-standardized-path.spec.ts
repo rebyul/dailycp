c38743723eee56a5177e5ea3950bc92738b2ee50
rebyul <rebyul@gmail.com>
2025-09-23 22:24:46 +1200

    shortest standardized path problem

---
+++ libs/dailycps/src/problems/sep-25/23-09/shortest-standardized-path.spec.ts
@@ -0,0 +1,29 @@
+import { shortestStandardizedPath } from './shortest-standardized-path';
+
+describe('Shortest standardized path', () => {
+  test('empty path returns empty string', () => {
+    expect(shortestStandardizedPath('')).toEqual('');
+  });
+
+  test('root path returns root path', () => {
+    expect(shortestStandardizedPath('/')).toEqual('/');
+  });
+
+  test('single path returns single path', () =>
+    expect(shortestStandardizedPath('/usr')).toEqual('/usr'));
+
+  test('path with . correctly truncates', () => {
+    expect(shortestStandardizedPath('/usr/.')).toEqual('/usr');
+  });
+
+  test('path with .. correctly traverses up', () => {
+    expect(shortestStandardizedPath('/usr/../test')).toEqual('/test');
+  });
+
+  test.each([['/usr/bin/../bin/./scripts/../', '/usr/bin/']])(
+    'given %s, shortestStandardizedPath returns %s',
+    (given, want) => {
+      expect(shortestStandardizedPath(given)).toEqual(want);
+    }
+  );
+});
---
+++ libs/dailycps/src/problems/sep-25/23-09/shortest-standardized-path.ts
@@ -0,0 +1,27 @@
+/**
+ * This problem was asked by Quora.
+
+Given an absolute pathname that may have . or .. as part of it, return the shortest standardized path.
+
+For example, given "/usr/bin/../bin/./scripts/../", return "/usr/bin/".
+*/
+
+export function shortestStandardizedPath(path: string): string {
+  const visited: string[] = [];
+  if (path.length === 0) return '';
+
+  const tokens = path.split('/');
+  for (const t of tokens) {
+    if (t === '.') {
+      continue;
+    }
+
+    if (t === '..') {
+      visited.pop();
+      continue;
+    }
+    visited.push(t);
+  }
+
+  return visited.join('/');
+}
